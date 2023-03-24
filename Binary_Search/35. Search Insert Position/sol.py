2b1f9385f288bc0aa6c61e52f6ed0c02bb7ef968
Matthew <matthewjcrosby1@gmail.com>
2023-03-24 12:44:18 +0700

    Change solution

--- Binary_Search/35. Search Insert Position/sol.py
+++ Binary_Search/35. Search Insert Position/sol.py
@@ -10,8 +10,6 @@ class Solution:
             if nums[mid] < target:
                 left = mid + 1
             else: right = mid - 1
-        if result == 0 and nums[result] > target:
-            return 0
-        if nums[result] < target:
-            return result + 1
-        else: return result
+        return left
+
+        
