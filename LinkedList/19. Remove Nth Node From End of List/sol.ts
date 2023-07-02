ad11086993874a14b5ce21005b04329e37176155
Matthew <matthewjcrosby1@gmail.com>
2023-07-02 10:26:06 +0700

    19

---
+++ LinkedList/19. Remove Nth Node From End of List/sol.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  let slowP = head;
+  let fastP = head;
+
+  for (let i = 0; i < n; i++) {
+    fastP = fastP.next;
+  }
+
+  if (!fastP) return head.next;
+
+  while (fastP.next) {
+    slowP = slowP.next;
+    fastP = fastP.next;
+  }
+
+  slowP.next = slowP.next.next;
+  return head;
+}
