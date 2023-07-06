37153a23d2df7c8258adfa089b061012723d8bbf
Matthew <matthewjcrosby1@gmail.com>
2023-07-06 15:39:35 +0700

    121

---
+++ SlidingWindow/121. Best Time to Buy and Sell Stock/sol.ts
@@ -0,0 +1,22 @@
+function maxProfit(prices: number[]): number {
+  let l = 0;
+  let r = l + 1;
+  let res = 0;
+  let runningTotal = 0;
+  let lowestNum = prices[l];
+  while (r < prices.length) {
+    const left = prices[l];
+    const right = prices[r];
+    const total = right - left;
+ /*    if (total > runningTotal) {
+      runningTotal = total;
+    }
+    if (lowestNum > prices[r]) {
+      lowestNum = prices[r];
+      l = r;
+    }
+    r++;
+  }
+  return runningTotal;
+}
+ */
