8f7714ada79e4d97bb4ef7d518080f5add974556
Matthew <matthewjcrosby1@gmail.com>
2023-07-09 15:44:00 +0700

    485

---
+++ SlidingWindow/485. Max Consecutive Ones/sol.ts
@@ -0,0 +1,15 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let left = 0;
+  let right = 0;
+
+  let best = 0;
+
+  while (right < nums.length) {
+    if (nums[right] === 0) {
+      left = right + 1;
+    }
+    best = Math.max(right - left + 1, best);
+    right++;
+  }
+  return best;
+}
